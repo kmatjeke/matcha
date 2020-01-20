fc96bc52c2d7efa153876ffa460c920c13c6a8f3
Koketso MATJEKE <kmatjeke@c5r5s7.wethinkcode.co.za>
2020-01-20 12:30:35 +0200

    database connected

--- config/setup.js
+++ config/setup.js
@@ -10,4 +10,8 @@ connection.connect();
 
 connection.query('CREATE DATABASE IF NOT EXISTS matcha');
 console.log('Database Created');
+connection.query('CREATE TABLE IF NOT EXISTS users(id INT(11) AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, Firstname VARCHAR(255) NOT NULL, Lastname VARCHAR(255) NOT NULL, Email VARCHAR(255) NOT NULL, Password VARCHAR(255) NOT NULL, Orientation VARCHAR(25), Age INT(9), City VARCHAR(255), Bio VARCHAR(1000), Hobbies VARCHAR(500), PRIMARY KEY (id))');
+console.log('table users created');
+connection.query('CREATE TABLE IF NOT EXISTS user_interests(id INT(11) AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, Orientation VARCHAR(25), Age_min INT(9), Age_max VARCHAR(25), City VARCHAR(255), Hobbies VARCHAR(500), PRIMARY KEY (id))');
+console.log('table user_interests created');
 connection.end();
--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const server = express();
 var connection = require('./config/db'),
     setup = require('./config/setup');
 
-server.listen('3000', function(){
+// server.get('/createpoststable', (req, res) => {
+//     let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+//     connection.query(sql, (err, result) => {
+//        if (err) {
+//            throw err;
+//        }
+//        else{
+//            console.log(result);
+//            res.send('Posts table created...');
+//        }
+//     });
+// });
+
+    server.listen('3000', function(){
     console.log('Listening on port 3000');
 });
 
