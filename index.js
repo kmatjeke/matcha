6de95f3081cf2b879ddb0b4d06b2955ce263e372
kmatjeke <kmatjeke@student.wethinkcode.co.za>
2020-03-09 20:40:40 +0200

    started login system

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const server = express();
-
-var connection = require('./config/db'),
-    setup = require('./config/setup');
+const bcrypt = require('bcrypt');
+const users = [];
+//var connection = require('./config/db'),
+  //  setup = require('./config/setup');
 
 // server.get('/createpoststable', (req, res) => {
 //     let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
@@ -16,8 +17,45 @@ var connection = require('./config/db'),
 //        }
 //     });
 // });
+    server.set('view-engine', 'ejs')
+    server.use(express.urlencoded({ extended: false}))
+    server.get('/', (req, res) => {
+      res.render("index.ejs", { name: 'Koketso'});
+    })
 
-    server.listen('3000', function(){
-    console.log('Listening on port 3000');
+    server.get('/login', (req, res) => {
+      res.render("login.ejs");
+    })
+
+    server.get('/register', (req, res) => {
+      res.render("register.ejs");
+    })
+
+    server.post('/register', async (req, res) => {
+      try {
+          const hashedPassword = await bcrypt.hash(req.body.password, 10)
+          users.push({
+            id: Date.now().toString,
+            username: req.body.username,
+            firstname: req.body.fname,
+            surname: req.body.sname,
+            email: req.body.email,
+            password: hashedPassword
+          })
+          res.redirect('/login');
+      } catch {
+          res.redirect('/register');
+      }
+      console.log(users);
+    });
+
+    server.post('/login', (req, res) => {
+      
+    });
+
+
+
+    server.listen('2000', function(){
+    console.log('Listening on port http://localhost:8000');
 });
 
